6362b6a0a5a104286b299742daadb11c2b4ce55a
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2016-11-19 03:34:59 +0200

    Add installer_specs and gemlist_server_specs
      - add specs for gemlist_server, installer
      - fix rubocop issues

--- spec/gemlist_server_spec.rb
+++ spec/gemlist_server_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
 describe GemListServer do
-  it 'should return correct dependencies as array' do
-    data = ['nvim-pkg', 'tmux-pkg', 'docker-pkg']
-    allow(GemListServer).to receive(:get).with(any_args).and_return(data)
+  context '#dependencies' do
+    let(:packages) { ['nvim-pkg', 'tmux-pkg', 'docker-pkg'] }
 
-    expect(GemListServer.new(['my_dev_setup']).dependencies).to eq(data)
+    before do
+      allow(GemListServer).to receive(:get).with(any_args).and_return(packages)
+    end
+
+    it 'should return correct dependencies as array' do
+      expect(GemListServer.new(['my_dev_setup']).dependencies).to eq(packages)
+    end
+
+    it 'should return only not installed dependencies' do
+      allow_any_instance_of(GemListServer).to receive(:pkg_exists?).with(packages[0]).and_return(false)
+      allow_any_instance_of(GemListServer).to receive(:pkg_exists?).with(packages[1]).and_return(true)
+      allow_any_instance_of(GemListServer).to receive(:pkg_exists?).with(packages[2]).and_return(true)
+
+      expect(GemListServer.new(['my_dev_setup']).dependencies).to eq([packages[0]])
+    end
   end
 end
--- spec/installer_spec.rb
+++ spec/installer_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe Installer do
+  it 'should return success and faild packages' do
+    allow(OsDetector).to receive(:current_os).and_return('macosx')
+    data = %w(vim nano)
+
+    allow(Installer).to receive(:macosx_install)
+      .with(data[0]).and_return(true)
+
+    allow(Installer).to receive(:macosx_install)
+      .with(data[1]).and_return(false)
+
+    expect(Installer.install(data)).to eq(success: [data[0]], fail: [data[1]])
+  end
+
   context '#macosx' do
     it 'should install packages if not installed' do
       allow(OsDetector).to receive(:current_os).and_return('macosx')
@@ -10,19 +23,6 @@ describe Installer do
         .with(any_args).and_return(true)
 
       expect(Installer.install(data)).to eq(success: data, fail: [])
-    end
-
-    it 'should return success and faild packages' do
-      allow(OsDetector).to receive(:current_os).and_return('macosx')
-      data = %w(vim nano)
-
-      allow(Installer).to receive(:macosx_install)
-        .with(data[0]).and_return(true)
-
-      allow(Installer).to receive(:macosx_install)
-        .with(data[1]).and_return(false)
-
-      expect(Installer.install(data)).to eq(success: [data[0]], fail: [data[1]])
     end
   end
 
