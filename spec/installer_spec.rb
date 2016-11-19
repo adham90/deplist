b70add6ebb1f0bae6c6b8135c1b9402ed297ec6e
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2016-11-19 02:44:39 +0200

    Add installer specs

---
+++ spec/installer_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+describe Installer do
+  context '#macosx' do
+    it "should install packages if not installed" do
+      allow(OsDetector).to receive(:current_os).and_return('macosx')
+      data = ['vim', 'nano']
+
+      allow(Installer).to receive(:macosx_install)
+        .with(any_args).and_return(true)
+
+      expect(Installer.install(data)).to eq({ success: data, fail: [] })
+    end
+
+    it 'should return success and faild packages' do
+      allow(OsDetector).to receive(:current_os).and_return('macosx')
+      data = ['vim', 'nano']
+
+      allow(Installer).to receive(:macosx_install)
+        .with(data[0]).and_return(true)
+
+      allow(Installer).to receive(:macosx_install)
+        .with(data[1]).and_return(false)
+
+      expect(Installer.install(data)).to eq({ success: [data[0]], fail: [data[1]] })
+    end
+  end
+
+  context '#linux' do
+    it "should install packages if not installed" do
+      allow(OsDetector).to receive(:current_os).and_return('linux')
+      data = ['vim', 'nano']
+
+      allow(Installer).to receive(:linux_install)
+        .with(any_args).and_return(true)
+
+      expect(Installer.install(data)).to eq({ success: data, fail: [] })
+    end
+  end
+end
